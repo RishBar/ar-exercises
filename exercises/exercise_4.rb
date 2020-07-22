654d6214c6f5932dd78278d116b8434d60ffa38b
Rishav Baral <rishavbaral6@gmail.com>
2020-07-22 08:09:37 +0000

    finish first 5 questions

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,29 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+store1 = Store.create do |u|
+  u.name = "Surrey"
+  u.annual_revenue = 224000
+  u.mens_apparel = false
+  u.womens_apparel = true
+end
+store2 = Store.create do |u|
+  u.name = "Whistler"
+  u.annual_revenue = 1900000
+  u.mens_apparel = true
+  u.womens_apparel = false
+end
+store2 = Store.create do |u|
+  u.name = "Yaletown"
+  u.annual_revenue = 430000
+  u.mens_apparel = true
+  u.womens_apparel = true
+end
+
+men_stores = Store.where(mens_apparel: true, womens_apparel: false)
+
+men_stores.each { |store| puts "NAME: #{store.name}, REVENUE: #{store.annual_revenue}" }
+
+women_stores = Store.where(mens_apparel: false, womens_apparel: true).where('annual_revenue > ?', 1000000)
+
+women_stores.each { |store| puts "NAME: #{store.name}, REVENUE: #{store.annual_revenue}" }
